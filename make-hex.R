13b85011dd837779201d300f31a388bc065d40c0
Andrew Collier <andrew@exegetic.biz>
2019-05-18 09:20:41 +0200

    First attempt at hex sticker.

---
+++ make-hex.R
@@ -0,0 +1,11 @@
+library(hexSticker)
+
+imgurl <- "docs/envelope-regular.svg"
+sticker(imgurl,
+        package="{imeyili}",
+        p_size=8,
+        s_x=1,
+        s_y=.75,
+        s_width=.6,
+        spotlight = TRUE,
+        filename="imeyili-hex.png")
