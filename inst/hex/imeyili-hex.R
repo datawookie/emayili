0d6fe79bfd7b6ee284ec916f7c7f62709eb6e6b3
Andrew Collier <andrew@exegetic.biz>
2019-05-18 13:01:51 +0200

    Improved hex sticker.

---
+++ inst/hex/imeyili-hex.R
@@ -0,0 +1,31 @@
+library(hexSticker)
+library(showtext)
+
+# Load Google font.
+#
+font_add_google("Roboto Slab", "roboto_slab")
+showtext_auto()
+
+sticker(here::here("inst/hex/envelope-regular.png"),
+        # Image
+        s_x = 1,
+        s_y = 1.10,
+        s_width = 0.6,
+        s_height = 0.6,
+        # Package name
+        package = "{imeyili}",
+        p_size = 16,
+        p_y = 0.45,
+        p_color = "#ffffff",
+        p_family = "roboto_slab",
+        # Heck
+        h_fill = "#3498db",
+        h_color = "#000000",
+        # Spotlight
+        spotlight = TRUE,
+        l_y = 0.45,
+        l_alpha = 0.4,
+        # Output
+        filename = here::here("inst/hex/imeyili-hex.png"),
+        dpi = 300
+)
