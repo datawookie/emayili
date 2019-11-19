6f09d8fa468dbb1560da087dbf504d5293fe01f3
Andrew Collier <andrew@exegetic.biz>
2019-11-19 11:10:18 +0200

    Clean up insertion of CID.

--- R/mime.R
+++ R/mime.R
@@ -20,18 +20,18 @@ mime <- function(content_type, encoding, format, charset, ...) {
 #'
 #' @return A formatted header string.
 format.mime <- function(msg) {
-  with(msg$header,
-       c(
-         'Content-Type: {content_type}',
-         ifelse(exists("charset") && !is.null(charset), '; charset="{charset}"', ''),
-         ifelse(exists("name"), '; name="{name}"', ''),
-         '\nContent-Disposition: ',
-         ifelse(grepl("text|image", content_type), 'inline', 'attachment'),
-         ifelse(exists("filename"), '; filename="{filename}"', ''),
-         ifelse(exists("cid"), '\nContent-Id: <{cid}>', ''),
-         ifelse(exists("cid"), '\nX-Attachment-Id: {cid}', ''),
-         '\nContent-Transfer-Encoding: {encoding}'
-       ) %>% paste(collapse = "") %>% glue()
+  headers <-  with(msg$header, c(
+    'Content-Type: {content_type}',
+    ifelse(exists("charset") && !is.null(charset), '; charset="{charset}"', ''),
+    ifelse(exists("name"), '; name="{name}"', ''),
+    '\nContent-Disposition: ',
+    ifelse(grepl("text|image", content_type), 'inline', 'attachment'),
+    ifelse(exists("filename"), '; filename="{filename}"', ''),
+    ifelse(exists("cid"), '\nContent-Id: <{cid}>\nX-Attachment-Id: {cid}', ''),
+    '\nContent-Transfer-Encoding: {encoding}'
   ) %>%
-    paste(msg$body, sep = "\n\n")
+    paste(collapse = "") %>%
+    glue())
+
+  paste(headers, msg$body, sep = "\n\n")
 }
