d2116a73454b2d64dc130a4abb7374396dd6e3ce
Andrew Collier <andrew@exegetic.biz>
2019-12-04 19:47:20 +0200

    Add test script to repository

---
+++ test.R
@@ -0,0 +1,21 @@
+library(dplyr)
+library(emayili)
+
+SMTP_USERNAME = Sys.getenv("SMTP_USER")
+SMTP_PASSWORD = Sys.getenv("SMTP_PASSWORD")
+SMTP_SERVER = Sys.getenv("SMTP_SERVER")
+SMTP_PORT = Sys.getenv("SMTP_PORT")
+
+smtp <- server(host = SMTP_SERVER, port = SMTP_PORT, username = SMTP_USERNAME, password = SMTP_PASSWORD)
+
+email <- envelope() %>%
+  from(SMTP_USERNAME) %>%
+  to(SMTP_USERNAME) %>%
+  subject("subject") %>%
+  body("Hello!") %>%
+  # attachment(c("README.md", "README.Rmd"))
+  attachment(c("README.md", "README.Rmd", "attachment-image.jpg"), cid = c("readme", NA, "image"))
+
+smtp(email, verbose = TRUE)
+
+cat(emayili:::message(email))
