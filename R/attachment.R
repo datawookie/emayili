f91363b37265a7c18bdad0a1d28a76409aeacde7
Andrew Collier <andrew@exegetic.biz>
2019-12-04 20:17:37 +0200

    Only accept single file per call to attachment()

--- R/attachment.R
+++ R/attachment.R
@@ -1,42 +1,39 @@
 #' Add attachments to a message object
 #'
 #' @param msg A message object.
-#' @param path Paths to files.
-#' @param cid A vector of the same length as \code{path} containing for each attachment either \code{NA} or a Content ID.
+#' @param path Path to file.
+#' @param cid Content-ID or \code{NA}.
 #' @return A message object.
 #' @export
 #' @examples
 #' \dontrun{
 #' msg <- envelope()
 #' attachment(msg, "report.xlsx")
-#' attachment(msg, c("visualisations.png", "report.pdf"), c("visuals", NA))
+#' attachment(msg, "cat.png")
+#' attachment(msg, "visualisations.png", "visuals")
 #' }
-attachment <- function(msg, path, cid = NULL){
-  types <- guess_type(path, empty = NULL)
+attachment <- function(msg, path, cid = NA){
+  if (length(path) != 1) stop("Must be precisely one attachment.", call. = F)
 
-  if(missing(cid) || is.null(cid)) cid = rep(NA, length(path))
-  else if(max(table(na.omit(cid))) > 1) stop("Duplicate CIDs were found. Please provide unique identifiers.", call. = F)
-  if(length(cid)!= length(path)) stop ("cid must be of the same length as path. Use NA if no ID should be assigned to a specific attachment.", call. = F)
+  type <- guess_type(path, empty = NULL)
 
-  for (i in seq_along(path)) {
-    con <- file(path[i], "rb")
-    body <- readBin(con, "raw",  file.info(path[i])$size)
-    close(con)
+  con <- file(path, "rb")
+  body <- readBin(con, "raw",  file.info(path)$size)
+  close(con)
 
-    mime <- mime(
-      types[i],
-      "base64",
-      NULL,
-      NULL,
-      name = basename(path[i]),
-      filename = basename(path[i]),
-      cid = as.character(cid[i])
-    )
+  mime <- mime(
+    type,
+    "base64",
+    NULL,
+    NULL,
+    name = basename(path),
+    filename = basename(path),
+    cid = as.character(cid)
+  )
 
-    mime$body <- base64encode(body, 76L, "\r\n")
+  mime$body <- base64encode(body, 76L, "\r\n")
 
-    msg$parts <- c(msg$parts, list(mime))
-  }
+  msg$parts <- c(msg$parts, list(mime))
 
   invisible(msg)
 }
