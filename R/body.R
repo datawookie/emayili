4d10ba5648f132363427dc384f09f77b3cfe49fa
Andrew Collier <andrew@exegetic.biz>
2019-12-14 16:00:01 +0200

    Replace body() with text() and html()

--- R/body.R
+++ R/body.R
@@ -1,29 +1,44 @@
-#' Add a body to a message object
+#' Add a text body to a message object
 #'
 #' @param msg A message object.
 #' @param content A string of message content.
-#' @param type Content type (plain text or HTML).
 #' @return A message object.
 #' @export
 #' @examples
 #' msg <- envelope()
-#' body(msg, "Hello!")
-#' body(msg, "<b>Hello!</b>", "html")
-body <- function(msg, content = NULL, type = c("plain", "html")){
-  type = type[1]
-
+#' text(msg, "Hello!")
+text <- function(msg, content = NULL) {
   if (is.null(content)) {
     msg$body
   } else {
-    type <- paste("text", type, sep = "/")
+    type <- "text/plain"
 
-    if (type == "text/plain") {
-      body <- mime(type, "inline", "7bit", "flowed", "utf-8")
-      body$body <- content
-    } else {
-      body <- mime(type, "inline", "quoted-printable", NULL, "utf-8")
-      body$body <- qp_encode(content)
-    }
+    body <- mime(type, "inline", "7bit", "flowed", "utf-8")
+    body$body <- content
+
+    msg$parts <- c(msg$parts, list(body))
+
+    invisible(msg)
+  }
+}
+
+#' Add an HTML body to a message object
+#'
+#' @param msg A message object.
+#' @param content A string of message content.
+#' @return A message object.
+#' @export
+#' @examples
+#' msg <- envelope()
+#' html(msg, "<b>Hello!</b>")
+html <- function(msg, content = NULL) {
+  if (is.null(content)) {
+    msg$body
+  } else {
+    type <- "text/html"
+
+    body <- mime(type, "inline", "quoted-printable", NULL, "utf-8")
+    body$body <- qp_encode(content)
 
     msg$parts <- c(msg$parts, list(body))
 
