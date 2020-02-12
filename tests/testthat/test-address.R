2ad9e61742ad104bc706b0b0ea7aa6d99d94b580
Andrew Collier <andrew_collier@derivco.co.za>
2020-02-12 08:22:28 +0000

    Added tests for more coverage

--- tests/testthat/test-address.R
+++ tests/testthat/test-address.R
@@ -1,4 +1,10 @@
-test_that("to: set from individual arguments", {
+test_that("to: set/get", {
+  msg <- envelope() %>% to("bob@gmail.com")
+  expect_equal(msg$header$To, c("bob@gmail.com"))
+  expect_equal(to(msg), "bob@gmail.com")
+})
+
+test_that("to: set multiple recipients", {
   msg <- envelope() %>% to("bob@gmail.com", "alice@yahoo.com")
   expect_equal(msg$header$To, c("bob@gmail.com", "alice@yahoo.com"))
 })
@@ -8,12 +14,19 @@ test_that("to: set from vector", {
   expect_equal(msg$header$To, c("bob@gmail.com", "alice@yahoo.com"))
 })
 
-test_that("from: set", {
+test_that("from: set/get", {
   msg <- envelope() %>% from("craig@gmail.com")
   expect_equal(msg$header$From, "craig@gmail.com")
+  expect_equal(from(msg), "craig@gmail.com")
 })
 
-test_that("cc: set from individual arguments", {
+test_that("cc: set/get", {
+  msg <- envelope() %>% cc("bob@gmail.com")
+  expect_equal(msg$header$Cc, c("bob@gmail.com"))
+  expect_equal(cc(msg), "bob@gmail.com")
+})
+
+test_that("cc: set multiple recipients", {
   msg <- envelope() %>% cc("bob@gmail.com", "alice@yahoo.com")
   expect_equal(msg$header$Cc, c("bob@gmail.com", "alice@yahoo.com"))
 })
@@ -23,7 +36,13 @@ test_that("cc: set from vector", {
   expect_equal(msg$header$Cc, c("bob@gmail.com", "alice@yahoo.com"))
 })
 
-test_that("bcc: set from individual arguments", {
+test_that("bcc: set/get", {
+  msg <- envelope() %>% bcc("bob@gmail.com")
+  expect_equal(msg$header$Bcc, c("bob@gmail.com"))
+  expect_equal(bcc(msg), c("bob@gmail.com"))
+})
+
+test_that("bcc: set  multiple recipients", {
   msg <- envelope() %>% bcc("bob@gmail.com", "alice@yahoo.com")
   expect_equal(msg$header$Bcc, c("bob@gmail.com", "alice@yahoo.com"))
 })
@@ -33,7 +52,8 @@ test_that("bcc: set from vector", {
   expect_equal(msg$header$Bcc, c("bob@gmail.com", "alice@yahoo.com"))
 })
 
-test_that("reply: set from individual arguments", {
+test_that("reply: set/get", {
   msg <- envelope() %>% reply("craig@gmail.com")
   expect_equal(msg$header$Reply, "craig@gmail.com")
+  expect_equal(reply(msg), c("craig@gmail.com"))
 })
