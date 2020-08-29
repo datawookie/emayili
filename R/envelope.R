b94c1053f68d5fd6685f0dd89960ce493a882a75
Andrew Collier <andrew@exegetic.biz>
2020-08-29 17:07:24 +0000

    Add arguments to envelope() which specify email attributes

--- R/envelope.R
+++ R/envelope.R
@@ -1,13 +1,33 @@
 #' Create a message.
 #'
+#' @param to See \code{to()}
+#' @param from See \code{from()}
+#' @param subject See \code{subject()}
+#' @param text See \code{text()}
+#' @param html See \code{html()}
+#'
 #' @return A message object.
 #' @seealso \code{\link{subject}}, \code{\link{from}}, \code{\link{to}}, \code{\link{cc}}, \code{\link{bcc}} and \code{\link{reply}}
 #' @export
 #' @examples
 #' # Create an (empty) message object.
 #' msg <- envelope()
-envelope <- function() {
-  structure(
+#'
+#' # Create a complete message object.
+#' envelope(
+#'   to = "bob@gmail.com",
+#'   from = "craig@gmail.com",
+#'   subject = "Hiya!",
+#'   text = "Hi Bob, how are you?"
+#' )
+envelope <- function(
+  to = NULL,
+  from = NULL,
+  subject = NULL,
+  text = NULL,
+  html = NULL
+) {
+  koevert <- structure(
     list(
       header = list(
         Date = http_date(Sys.time())
@@ -16,8 +36,15 @@ envelope <- function() {
       parts = list()
     ),
     class="envelope")
-}
 
+  if (!is.null(to)) koevert <- to(koevert, to)
+  if (!is.null(from)) koevert <- from(koevert, from)
+  if (!is.null(subject)) koevert <- subject(koevert, subject)
+  if (!is.null(text)) koevert <- text(koevert, text)
+  if (!is.null(html)) koevert <- html(koevert, html)
+
+  koevert
+}
 
 #' Print a message object
 #'
