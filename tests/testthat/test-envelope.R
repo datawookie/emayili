8e55f0e91d0a0a9493d77e979379d819918f28d6
Matt Dennis <mattdenni@gmail.com>
2020-08-31 16:24:05 +0200

    Added tests for new envelope() functionality

--- tests/testthat/test-envelope.R
+++ tests/testthat/test-envelope.R
@@ -2,3 +2,35 @@ test_that("envelope print() output", {
   msg <- envelope() %>% subject("Test message")
   expect_output(print(msg), "Date:         .*\nSubject:      Test message")
 })
+
+test_that("class envelope", {
+  object <- envelope()
+  expect_output(print(class(object)), "envelope")
+})
+
+test_that("recipient address", {
+  recipient <- envelope(to = "bob@gmail.com")
+  expect_equal(recipient$header$To, "bob@gmail.com")
+})
+
+test_that("sender address", {
+  sender <- envelope(from = "bob@gmail.com")
+  expect_equal(sender$header$From, "bob@gmail.com")
+})
+
+test_that("subject", {
+  subject <- envelope(subject = "Email Subject")
+  expect_equal(subject$header$Subject, "Email Subject")
+})
+
+test_that("body text", {
+  email_text <- envelope(text = "foo")
+  expect_equal(email_text$parts[[1]]$body, "foo")
+})
+
+test_that("body html", {
+  html <- envelope(html = "foo")
+  expect_equal(html$parts[[1]]$body, "foo")
+})
+
+
