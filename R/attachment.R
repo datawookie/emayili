b05f2b3945ce113740b8daf28e253b755477efb3
Andrew Collier <andrew@fathomdata.dev>
2021-07-30 06:26:53 +0300

    fix: Set default disposition to "attachment" & only guess if explicitly set to NA

--- R/attachment.R
+++ R/attachment.R
@@ -5,7 +5,7 @@
 #' @param name Name to be used for attachment (defaults to base name of \code{path}).
 #' @param type MIME type or \cite{NA}, which will result in a guess based on file extension.
 #' @param cid Content-ID or \code{NA}.
-#' @param disposition Should the content be displayed inline or as an attachment?
+#' @param disposition Should the content be displayed inline or as an attachment? Valid options are \code{"inline"} and \code{"attachment"}. If set to \code{NA} then will guess appropriate value.
 #' @return A message object.
 #' @export
 #' @examples
@@ -28,7 +28,7 @@
 #'   attachment(path_scatter, cid = "scatter")
 #'
 #' file.remove(path_scatter, path_mtcars)
-attachment <- function(msg, path, name = NA, type = NA, cid = NA, disposition = NA) {
+attachment <- function(msg, path, name = NA, type = NA, cid = NA, disposition = "attachment") {
     if (length(path) != 1)
       stop("Must be precisely one attachment.", call. = F)
 
