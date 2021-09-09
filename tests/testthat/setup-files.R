de882a90b839aa687e795fa07a48a5d73aedb34d
Andrew Collier <andrew@fathomdata.dev>
2021-09-09 09:17:14 +0100

    chore: Add tests for render

--- tests/testthat/setup-files.R
+++ tests/testthat/setup-files.R
@@ -1,3 +1,7 @@
+library(logger)
+
+log_threshold(ERROR)
+
 TXTPATH <- tempfile(fileext = ".txt")
 #
 writeLines("Some random text.", TXTPATH)
@@ -16,6 +20,20 @@ writeLines(HTMLCONTENT, HTMLPATH)
 # This file comes from https://bit.ly/2P4LUO8 (cat poster on WikiPedia).
 #
 JPGPATH <- here::here("inst", "cats.jpg")
+
+# R MARKDOWN FILE --------------------------------------------------------------
+
+FILE_RMD <- "message.Rmd"
+
+# Create an Rmd document from template.
+rmarkdown::draft(
+  FILE_RMD,
+  template = "github_document",
+  package = "rmarkdown",
+  edit = FALSE
+)
+
+# ------------------------------------------------------------------------------
 
 SMTP_USERNAME <- Sys.getenv("SMTP_USERNAME")
 SMTP_PASSWORD <- Sys.getenv("SMTP_PASSWORD")
