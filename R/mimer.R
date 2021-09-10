18cda466c59bd70b7ce237a65ab619bf28113fe9
Andrew Collier <andrew@fathomdata.dev>
2021-09-10 02:51:26 +0100

    Add base classes

---
+++ R/mimer.R
@@ -0,0 +1,40 @@
+# MIME
+#  ├── mixed
+#  └── related
+#
+MIME <- function() {
+  structure(
+    list(
+    ),
+    class = "MIME"
+  )
+}
+
+related <- function() {
+  mime <- MIME()
+  structure(
+    list(
+    ),
+    class = c("related", "MIME")
+  )
+}
+
+mixed <- function() {
+  structure(
+    list(
+    ),
+    class = c("mixed", "MIME")
+  )
+}
+
+format.MIME <- function(x) "MIME"
+format.related <- function(x) c("related", NextMethod())
+format.mixed <- function(x) c("mixed", NextMethod())
+
+x <- MIME()
+y <- related()
+z <- mixed()
+
+print(x)
+print(y)
+print(z)
