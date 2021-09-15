7090e135f1711ebb6ae5a444f06c2dd602647bd1
Andrew Collier <andrew@fathomdata.dev>
2021-09-15 10:07:50 +0100

    feat: Interpolate in subject()

--- tests/testthat/test-subject.R
+++ tests/testthat/test-subject.R
@@ -2,3 +2,10 @@ test_that("subject: set/get", {
   msg <- envelope() %>% subject("Test message")
   expect_equal(subject(msg), "Test message")
 })
+
+test_that("interpolate", {
+  name <- "Alice"
+  msg <- envelope() %>% subject("Hello {{name}}!")
+
+  expect_equal(subject(msg), "Hello Alice!")
+})
