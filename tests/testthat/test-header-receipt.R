ca648243d07fc742a5c7ed4cf8a6d531587ff3db
Andrew Collier <andrew@fathomdata.dev>
2021-11-15 07:42:17 +0000

    feat: Tests for request_receipt_read()

---
+++ tests/testthat/test-header-receipt.R
@@ -0,0 +1,11 @@
+test_that("read receipt", {
+  expect_error(envelope() %>% request_receipt_read())
+
+  msg <- envelope() %>%
+    from("olivia@google.com") %>%
+    request_receipt_read()
+
+  expect_match(headers(msg), "Return-Receipt-To: +olivia@google.com")
+  expect_match(headers(msg), "Disposition-Notification-To: +olivia@google.com")
+  expect_match(headers(msg), "X-Confirm-Reading-To: +olivia@google.com")
+})
