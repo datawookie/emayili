0af17eda679d49a1b7eab7f4e2c6ad54587606a8
Andrew Collier <andrew@fathomdata.dev>
2021-11-23 08:00:41 +0000

    fix: Don't run {Microsoft365R} tests in CI

--- tests/testthat/test-compatibility-microsoft365r.R
+++ tests/testthat/test-compatibility-microsoft365r.R
@@ -1,31 +1,33 @@
+skip_on_ci()
+# skip_if_not(
+#   interactive(),
+#   "{Microsoft365R} tests skipped (not interactive session)."
+# )
 skip_if_not(
   require(Microsoft365R, quietly=TRUE),
   "{Microsoft365R} tests skipped (package not available)."
 )
-require(AzureAuth)
 
-tenant <- "consumers"
-app <- "d44a05d5-c6a5-4bbb-82d2-443123722380"
 from_addr <- "anne@example.com"
 to_addr <- "bob@example.com"
 cc_addr <- "jane@example.com"
 bcc_addr <- "rex@example.com"
 
-scopes <- c(
-  file.path("https://graph.microsoft.com", c("Mail.Send", "Mail.ReadWrite", "User.Read")),
-  "openid",
-  "offline_access"
-)
-token <- get_azure_token(
-  scopes,
-  tenant,
-  app,
-  version = 2
-)
+# require(AzureAuth)
+# scopes <- c(
+#   file.path("https://graph.microsoft.com", c("Mail.Send", "Mail.ReadWrite", "User.Read")),
+#   "openid",
+#   "offline_access"
+# )
+# token <- get_azure_token(
+#   scopes,
+#   "consumers",
+#   "d44a05d5-c6a5-4bbb-82d2-443123722380",
+#   version = 2
+# )
+# outlook <- get_personal_outlook(token = token)
 
-outlook <- get_personal_outlook(token = token)
-
-folder <- outlook$create_folder(rndchar())
+outlook <- get_personal_outlook()
 
 msg <- envelope(
   to = to_addr,
@@ -34,7 +36,7 @@ msg <- envelope(
   subject = SUBJECT,
   html = HTMLCONTENT
 )
-msg <- folder$create_email(msg)
+msg <- outlook$create_email(msg)
 
 expect_identical(msg$properties$body$contentType, "html")
 expect_true(grepl(HTMLCONTENT, msg$properties$body$content))
