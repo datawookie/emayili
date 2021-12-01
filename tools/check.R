479d818bc0a23dc6ff39397c7bdbe396d18427c4
Andrew Collier <andrew@fathomdata.dev>
2021-12-01 17:29:47 +0000

    feat: Add more thorough checks

--- tools/check.R
+++ tools/check.R
@@ -1,3 +1,17 @@
 spelling::spell_check_package()
 
+devtools::check(cran = TRUE)
+
+# Incoming feasibility checks for CRAN.
+#
+devtools::check(
+  manual = TRUE,
+  remote = TRUE,
+  incoming = TRUE
+)
+
+# Check it on Windows.
+#
+devtools::check_win_devel()
+
 rhub::check_for_cran()
