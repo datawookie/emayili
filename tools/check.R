2cb965524432ec1fcb7f96dcfe93cc5321eee618
Andrew Collier <andrew@fathomdata.dev>
2022-02-04 05:27:19 +0000

    feat: List of many platforms to test on

--- tools/check.R
+++ tools/check.R
@@ -16,7 +16,31 @@ devtools::check(
 #
 devtools::check_win_devel()
 
-rhub::check_for_cran()
+rhub::check_for_cran(
+  platforms = c(
+    # devel
+    "debian-clang-devel",
+    "debian-gcc-devel",
+    "ubuntu-gcc-devel",
+    "fedora-clang-devel",
+    "fedora-gcc-devel",
+    "windows-x86_64-devel",
+    # patched
+    "debian-gcc-patched",
+    "windows-x86_64-patched",
+    "solaris-x86-patched",
+    # release
+    "debian-gcc-release",
+    "ubuntu-gcc-release",
+    "macos-highsierra-release",
+    "macos-m1-bigsur-release",
+    "windows-x86_64-release"
+  ),
+  env_vars = c(
+    `_R_CHECK_FORCE_SUGGESTS_` = "false",
+    `_R_CHECK_CRAN_INCOMING_USE_ASPELL_` = "false"
+  )
+)
 
 # Check on macOS.
 #
