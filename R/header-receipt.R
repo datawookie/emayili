951dfb10e4c8f4eb16c219e5c2a811b7f692f5c7
Andrew Collier <andrew@fathomdata.dev>
2022-04-06 10:28:50 +0100

    refactor: Separate out function for delivery receipt

--- R/header-receipt.R
+++ R/header-receipt.R
@@ -1,19 +1,33 @@
-#' Request read receipt
+#' Request read or delivery receipts
 #'
 #' Request the recipient to acknowledge that they have read the message. Inserts
 #' MDN (Message Disposition Notification) header entries.
 #'
+#' @name receipt
+#'
 #' @param msg A message object.
-#' @param addr Single address.
+#' @param addr Single address (optional). If `address` is not specified then
+#'   will use sender address.
 #' @return A message object.
 #' @export
 request_receipt_read <- function(msg, addr = NULL) {
   if (is.null(addr)) addr <- from(msg)
   if (is.null(addr)) stop("Either specify receipt address or call from() first.")
 
-  msg <- header_set(msg, "Return-Receipt-To", as.address(addr), append = FALSE)
   msg <- header_set(msg, "Disposition-Notification-To", as.address(addr), append = FALSE)
   msg <- header_set(msg, "X-Confirm-Reading-To", as.address(addr), append = FALSE)
+
+  if (get_option_invisible()) invisible(msg) else msg # nocov
+}
+
+#' @rdname receipt
+#'
+#' @export
+request_receipt_delivery <- function(msg, addr = NULL) {
+  if (is.null(addr)) addr <- from(msg)
+  if (is.null(addr)) stop("Either specify receipt address or call from() first.")
+
+  msg <- header_set(msg, "Return-Receipt-To", as.address(addr), append = FALSE)
 
   if (get_option_invisible()) invisible(msg) else msg # nocov
 }
--- tests/testthat/test-header-receipt.R
+++ tests/testthat/test-header-receipt.R
@@ -5,7 +5,16 @@ test_that("read receipt", {
     from("olivia@google.com") %>%
     request_receipt_read()
 
-  expect_match(headers(msg), "Return-Receipt-To: +olivia@google.com")
   expect_match(headers(msg), "Disposition-Notification-To: +olivia@google.com")
   expect_match(headers(msg), "X-Confirm-Reading-To: +olivia@google.com")
+})
+
+test_that("delivery receipt", {
+  expect_error(envelope() %>% request_receipt_delivery())
+
+  msg <- envelope() %>%
+    from("olivia@google.com") %>%
+    request_receipt_delivery()
+
+  expect_match(headers(msg), "Return-Receipt-To: +olivia@google.com")
 })
