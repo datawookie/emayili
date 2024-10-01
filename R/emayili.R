d4c1fc023f6b6e6b1a1a6883835b19b31aca305d
smartiing <159499215+smartiing@users.noreply.github.com>
2024-10-01 21:29:27 +0200

    remove global option for millisecond time display in .onLoad().
    The options(digits.secs = 3) line was removed from the .onLoad function to avoid changing the default global time display settings to millisecond precision. This prevents unintended modifications to users' global options.

--- R/emayili.R
+++ R/emayili.R
@@ -26,7 +26,6 @@ NULL
 .onLoad <- function(libname, pkgname) {
   # nolint start
   # nocov start
-  options(digits.secs = 3)
   log_layout(layout_glue_generator('{stringr::str_pad(level, 7, side = "right")} [{time}] {msg}'))
   # nocov end
   # nolint end
