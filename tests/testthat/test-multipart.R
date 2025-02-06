d8f05e0f05a5fbd5442a46be4ee9a857832e730b
Andrew Collier <andrew@fathomdata.dev>
2025-02-06 07:14:24 +0000

    test: Send messages with different MIME combinations

--- tests/testthat/test-multipart.R
+++ tests/testthat/test-multipart.R
@@ -1,72 +1,106 @@
-# test_that("message with just text", {
-#   mime <- envelope() %>%
-#     text("Hello!") %>%
-#     as.character()
-#
-#   expect_no_match(mime, "multipart/alternative")
-#   expect_no_match(mime, "multipart/mixed")
-#   expect_match(mime, "Hello!$")
-# })
-#
-# test_that("message with just HTML", {
-#   mime <- envelope() %>%
-#     html("<p>Hello!</p>") %>%
-#     as.character()
-#
-#   expect_no_match(mime, "multipart/alternative")
-#   expect_no_match(mime, "multipart/mixed")
-#   expect_match(mime, "<html><body><p>Hello!</p></body></html>$")
-# })
-#
-# test_that("message with text and HTML", {
-#   mime <- envelope() %>%
-#     text("Hello!") %>%
-#     html("<p>Hello!</p>") %>%
-#     as.character()
-#
-#   expect_match(mime, "multipart/alternative")
-#   expect_no_match(mime, "multipart/mixed")
-#   expect_match(mime, "Hello!")
-#   expect_match(mime, "<html><body><p>Hello!</p></body></html>")
-# })
+msg <- envelope() %>%
+  from(EMAIL_FROM) %>%
+  to(EMAIL_TO)
+
+test_that("message with just text", {
+  msg <- msg %>%
+    subject("MIME: text") %>%
+    text("Hello!")
+
+  txt <- as.character(msg)
+
+  expect_no_match(txt, "multipart/alternative")
+  expect_no_match(txt, "multipart/mixed")
+  expect_match(txt, "Hello!$")
+
+  skip_on_cran()
+  expect_error(smtp_gmail(msg), NA)
+})
+
+test_that("message with just HTML", {
+  msg <- msg %>%
+    subject("MIME: HTML") %>%
+    html("<p>Hello!</p>")
+
+  txt <- as.character(msg)
+
+  expect_no_match(txt, "multipart/alternative")
+  expect_no_match(txt, "multipart/mixed")
+  expect_match(txt, "<html><body><p>Hello!</p></body></html>$")
+
+  skip_on_cran()
+  expect_error(smtp_gmail(msg), NA)
+})
+
+test_that("message with text and HTML", {
+  msg <- msg %>%
+    subject("MIME: text+HTML") %>%
+    text("Hello!") %>%
+    html("<p>Hello!</p>")
+
+  txt <- as.character(msg)
+
+  expect_match(txt, "multipart/alternative")
+  expect_no_match(txt, "multipart/mixed")
+  expect_match(txt, "Hello!")
+  expect_match(txt, "<html><body><p>Hello!</p></body></html>")
+
+  skip_on_cran()
+  expect_error(smtp_gmail(msg), NA)
+})
 
 test_that("message with just text and attachment", {
-  mime <- envelope() %>%
+  msg <- msg %>%
+    subject("MIME: text+attachment") %>%
     text("Hello!") %>%
-    attachment(TXTPATH) %>%
-    as.character()
+    attachment(TXTPATH)
 
-  expect_no_match(mime, "multipart/alternative")
-  expect_match(mime, "multipart/mixed")
-  expect_match(mime, "Content-Type: +text/plain;")
-  expect_match(mime, "Content-Disposition: +attachment;")
-  expect_match(mime, "Hello!")
+  txt <- as.character(msg)
+
+  expect_no_match(txt, "multipart/alternative")
+  expect_match(txt, "multipart/mixed")
+  expect_match(txt, "Content-Type: +text/plain;")
+  expect_match(txt, "Content-Disposition: +attachment;")
+  expect_match(txt, "Hello!")
+
+  skip_on_cran()
+  expect_error(smtp_gmail(msg), NA)
 })
 
 test_that("message with just HTML and attachment", {
-  mime <- envelope() %>%
+  msg <- msg %>%
+    subject("MIME: HTML+attachment") %>%
     html("<p>Hello!</p>") %>%
-    attachment(TXTPATH) %>%
-    as.character()
+    attachment(TXTPATH)
 
-  expect_no_match(mime, "multipart/alternative")
-  expect_match(mime, "multipart/mixed")
-  expect_match(mime, "Content-Type: +text/plain;")
-  expect_match(mime, "Content-Disposition: +attachment;")
-  expect_match(mime, "<html><body><p>Hello!</p></body></html>")
+  txt <- as.character(msg)
+
+  expect_no_match(txt, "multipart/alternative")
+  expect_match(txt, "multipart/mixed")
+  expect_match(txt, "Content-Type: +text/plain;")
+  expect_match(txt, "Content-Disposition: +attachment;")
+  expect_match(txt, "<html><body><p>Hello!</p></body></html>")
+
+  skip_on_cran()
+  expect_error(smtp_gmail(msg), NA)
 })
 
 test_that("message with text and HTML and attachment", {
-  mime <- envelope() %>%
+  msg <- msg %>%
+    subject("MIME: text+HTML+attachment") %>%
     text("Hello!") %>%
     html("<p>Hello!</p>") %>%
-    attachment(TXTPATH) %>%
-    as.character()
+    attachment(TXTPATH)
 
-  expect_no_match(mime, "multipart/alternative")
-  expect_match(mime, "multipart/mixed")
-  expect_match(mime, "Content-Type: +text/plain;")
-  expect_match(mime, "Content-Disposition: +attachment;")
-  expect_match(mime, "Hello!")
-  expect_match(mime, "<html><body><p>Hello!</p></body></html>")
+  txt <- as.character(msg)
+
+  expect_no_match(txt, "multipart/alternative")
+  expect_match(txt, "multipart/mixed")
+  expect_match(txt, "Content-Type: +text/plain;")
+  expect_match(txt, "Content-Disposition: +attachment;")
+  expect_match(txt, "Hello!")
+  expect_match(txt, "<html><body><p>Hello!</p></body></html>")
+
+  skip_on_cran()
+  expect_error(smtp_gmail(msg), NA)
 })
